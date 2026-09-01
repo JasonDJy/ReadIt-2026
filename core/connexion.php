3f9887608b6fd4607c64fb9a89fe94b96611564b
Delbovier Jason <jasondj2000@outlook.com>
2026-09-01 09:43:51 +0200

    Structure MVC initiale

---
+++ core/connexion.php
@@ -0,0 +1,8 @@
+<?php
+
+try {
+    $connexion = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PWD);
+} catch (PDOException $e) {
+    // tenter de réessayer la connexion après un certain délai, par exemple
+    echo $e->getMessage();
+}
