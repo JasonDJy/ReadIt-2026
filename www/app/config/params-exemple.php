800ab17de5d8826ddb069ddea95b188691fcffb7
Delbovier Jason <jasondj2000@outlook.com>
2026-09-08 12:51:20 +0200

    Routes users-login-form

---
+++ www/app/config/params-exemple.php
@@ -0,0 +1,10 @@
+<?php
+//Initialiser les zones dynamiques  
+$title = "";
+$content = "Oups, il semble y avoir un problème.";
+
+// Parametres de connexion
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PWD", "");
---
+++ www/app/controllers/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers\UsersController;
+
+use \PDO;
+// use \App\Models\UsersModel;
+// include_once '../app/models/usersModel.php';
+
+function loginFormAction(PDO $connexion)
+{
+    global $content, $title;
+    $title = "Login Form";
+    ob_start();
+    include '../app/views/users/loginForm.php';
+    $content = ob_get_clean();
+}
+
+function loginAction(PDO $connexion, array $userdata)
+{
+    var_dump($userdata);
+    die();
+}
