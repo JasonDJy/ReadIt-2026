e0d9048cfcf0c4a02453feadf08f20ecd95c1f00
Delbovier Jason <jasondj2000@outlook.com>
2026-09-15 10:49:52 +0200

    Découpe du template en partials

--- www/app/controllers/UsersController.php
+++ www/app/controllers/UsersController.php
@@ -3,8 +3,9 @@
 namespace App\Controllers\UsersController;
 
 use \PDO;
-// use \App\Models\UsersModel;
-// include_once '../app/models/usersModel.php';
+
+use \App\Models\UsersModel;
+include_once '../app/models/usersModel.php';
 
 function loginFormAction(PDO $connexion)
 {
@@ -17,6 +18,22 @@ function loginFormAction(PDO $connexion)
 
 function loginAction(PDO $connexion, array $userdata)
 {
-    var_dump($userdata);
-    die();
+    // Y a t-il une correspondance entre le login et le mot de passe dans la base de données ?
+
+    $user = UsersModel\findOneLoginAndPwd($connexion, $userdata);
+
+    // si aucune correspondance, on redirige vers le formulaire de login avec un message d'erreur
+
+    if ($user) {
+        // Les identifiants sont corrects
+        // On redirige vers la page d'accueil
+        header('Location: ' . ADMIN_BASE_URL);
+        exit();
+    } else {
+        // Les identifiants sont incorrects
+        // On redirige vers le formulaire de login avec un message d'erreur
+        header('Location: ' . PUBLIC_BASE_URL . '/users/login-form');
+        exit();
+    }
+
 }
---
+++ www/app/models/usersModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\UsersModel;
+
+use \PDO;
+
+function findOneLoginAndPwd (PDO $connexion, array $userData)
+{
+    $sql = "SELECT * FROM users WHERE login = :login AND pwd = :pwd";
+    $rs = $connexion->prepare($sql);
+    $rs->bindValue(':login', $userData['login'], PDO::PARAM_STR);
+    $rs->bindValue(':pwd', $userData['pwd'], PDO::PARAM_STR);
+    $rs->execute();
+    return $rs->fetch(PDO::FETCH_ASSOC);
+}
