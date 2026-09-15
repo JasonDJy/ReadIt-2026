a90a1fae9042b81e4f42040e86e8256e0b1c8b25
Delbovier Jason <jasondj2000@outlook.com>
2026-09-15 11:45:18 +0200

    Découpe du template en partials

--- back/app/views/templates/default.php
+++ back/app/views/templates/default.php
@@ -6,19 +6,11 @@
 </head>
 
 <body>
-
     <?php include '../app/views/templates/partials/_nav.php'; ?>
-
-    <?php include '../app/views/templates/partials/_hero.php'; ?>
 
     <?php include '../app/views/templates/partials/_main.php'; ?>
 
-    <?php include '../app/views/templates/partials/_footer.php'; ?>
-
-    <?php include '../app/views/templates/partials/_loader.php'; ?>
-
     <?php include '../app/views/templates/partials/_scripts.php'; ?>
-
 </body>
 
 </html>
--- back/app/views/templates/partials/_main.php
+++ back/app/views/templates/partials/_main.php
@@ -1,12 +1,3 @@
-<section class="ftco-section ftco-degree-bg">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 ftco-animate">
-                <?php echo $content; ?>
-            </div>
-
-            <?php include '../app/views/templates/partials/_aside.php'; ?>
-
-        </div>
-    </div>
-</section> <!-- .section -->
+<div class="container theme-showcase" role="main" class="row">
+    <?php echo $content; ?>
+</div>
